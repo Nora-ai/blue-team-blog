287f99a56229e16888cd7cb2d6e49b1c52c72fc4
kellyappiah <kellybappiah@gmail.com>
2020-07-14 14:44:06 -0400

    blog-update

--- client/src/services/ApiConfig.js
+++ client/src/services/ApiConfig.js
@@ -3,18 +3,18 @@ import axios from 'axios'
 let apiUrl
 
 const apiUrls = {
-    production: 'https://blue-team-blog-app.herokuapp.com/api',
-    development: 'http://localhost:3001/api'
+  production: 'https://blue-team-blog-app.herokuapp.com/api',
+  development: 'http://localhost:3001/api'
 }
 
 if (window.location.hostname === 'localhost') {
-    apiUrl = apiUrls.development
+  apiUrl = apiUrls.development
 } else {
-    apiUrl = apiUrls.production
+  apiUrl = apiUrls.production
 }
 
 const api = axios.create({
-    baseURL: apiUrl
+  baseURL: apiUrl
 })
 
 export default api
